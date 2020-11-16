e91fff99279b6b7680c54e4b8c9ba683b1bb79dc
gurami152 <gurami152@gmail.com>
2020-11-16 18:07:58 +0200

    added laboratory files

---
+++ test/android/src/com/drebezovdenis/lab6/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.drebezovdenis.lab6;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.drebezovdenis.lab6.lab6;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new lab6(), config);
+	}
+}
---
+++ test/desktop/src/com/drebezovdenis/lab6/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.drebezovdenis.lab6.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.drebezovdenis.lab6.lab6;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new lab6(), config);
+	}
+}
